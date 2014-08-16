33889548bcb188d9fd8078d7e276b7330b4634a0
Ray <Ray@Rays-MacBook-Pro.local>
2014-08-16 21:23:34 +0800

    [8/16] Added codes for keeping the scrollView position between pauses & resumes. Added xml files for 新生鍛造營 items.

--- java/com/Ray/i_shu/MainActivity.java
+++ java/com/Ray/i_shu/MainActivity.java
@@ -5,6 +5,8 @@ import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ScrollView;
+//import android.widget.AdapterView.OnItemClickListener;
 
 import java.util.ArrayList;
 
@@ -14,6 +16,8 @@ import java.util.ArrayList;
 public class MainActivity extends Activity {
     private multirowGridView _gridsWhatsNew, _gridsCamp, _gridsInfo;
     private GridViewAdapter _gridViewAdapter;
+    private ScrollView _scrollView;
+    private int _scrollX, _scrollY;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
@@ -21,23 +25,65 @@ public class MainActivity extends Activity {
         setContentView(R.layout.main_layout);
 
         getActionBar().setTitle(R.string.app_name);
+        _scrollView = (ScrollView) findViewById(R.id.main_scrollView);
         setMultirowGridViews();
         setMultirowGridViewListeners();
     }
 
+    @Override
+    public void onResume(){
+        super.onResume();
+        _scrollView.scrollTo(_scrollX, _scrollY);
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        _scrollX = _scrollView.getScrollX();
+        _scrollY = _scrollView.getScrollY();
+    }
+    // methods to keep _scrollView position
+    /*protected void onSaveInstanceState(Bundle outState){
+        outState.putIntArray("SCROLL_POSITION", new int[]{_scrollView.getScrollX(),
+                                                          _scrollView.getScrollY()});
+        super.onSaveInstanceState(outState);
+    }
+
+    protected void onRestoreInstanceState(Bundle savedInstanceState){
+        super.onRestoreInstanceState(savedInstanceState);
+        int[] position = savedInstanceState.getIntArray("SCROLL_POSITION");
+        if(position != null){
+            _scrollView.scrollTo(position[0],position[1]);
+        }
+    }*/
+
     private void setMultirowGridViewListeners(){
-        _gridsWhatsNew.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        /*_gridsWhatsNew.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
 
             }
+        });*/
+        _gridsCamp.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                switch(i){
+                    case 1://clicked camp_news
+                        break;
+                    case 4://clicked camp_activity
+                        break;
+                    case 5://clicked camp_album
+                        break;
+                    default://no function for other grid items.
+                        break;
+                }
+            }
         });
-        //_gridsCamp.setOnClickListener();
         //_gridsInfo.setOnClickListener();
     }
 
     private void setMultirowGridViews(){
-        _gridsWhatsNew = (multirowGridView) findViewById(R.id.whats_new_content);
+        _gridsWhatsNew = (multirowGridView) findViewById(R.id.whatsNew_content);
         _gridsCamp = (multirowGridView) findViewById(R.id.camp_content);
         _gridsInfo = (multirowGridView) findViewById(R.id.information_content);
         _gridViewAdapter = new GridViewAdapter(this, R.layout.grid_view, getNewsData());
